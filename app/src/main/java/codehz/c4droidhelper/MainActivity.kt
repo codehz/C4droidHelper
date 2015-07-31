61cd6c5ce01339b992d94019d41b7d801be33de3
CodeHz <codehz@outlook.com>
2015-07-31 20:50:49 +0800

    初始化

---
+++ app/src/main/java/codehz/c4droidhelper/MainActivity.kt
@@ -0,0 +1,11 @@
+package codehz.c4droidhelper
+
+import android.app.Activity
+import android.os.Bundle
+
+public class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
